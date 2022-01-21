cd21be4957f9c460cc63e0584a1d68755959c42e
pocoyo <67566122+Pocoyo-dev@users.noreply.github.com>
2022-01-20 22:55:50 -0500

    oregano

---
+++ src/Thnoogle/thnoogle.py
@@ -0,0 +1,275 @@
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtPrintSupport import *
+from PyQt5.QtGui import *
+
+import os
+import sys
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(
+        os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+class CustomWebEnginePage(QWebEnginePage):
+    """ Custom WebEnginePage to customize how we handle link navigation """
+    # Store external windows.
+    external_windows = []
+
+    def acceptNavigationRequest(self, url,  _type, isMainFrame):
+        if _type == QWebEnginePage.NavigationTypeLinkClicked:
+            w = QWebEngineView()
+            w.setUrl(url)
+            w.show()
+
+            # Keep reference to external window, so it isn't cleared up.
+            self.external_windows.append(w)
+            return False
+        return super().acceptNavigationRequest(url,  _type, isMainFrame)
+
+class AboutDialog(QDialog):
+    def __init__(self, *args, **kwargs):
+        super(AboutDialog, self).__init__(*args, **kwargs)
+
+        QBtn = QDialogButtonBox.Ok  # No cancel
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        self.buttonBox.rejected.connect(self.reject)
+
+        layout = QVBoxLayout()
+
+        title = QLabel("Thnoogle")
+        font = title.font()
+        font.setPointSize(20)
+        title.setFont(font)
+
+        layout.addWidget(title)
+
+        logo = QLabel()
+        logo.setPixmap(QPixmap(resource_path('images/icon.ico')))
+        layout.addWidget(logo)
+
+        layout.addWidget(QLabel("Version 02.03.20.12"))
+
+        for i in range(0, layout.count()):
+            layout.itemAt(i).setAlignment(Qt.AlignHCenter)
+
+        layout.addWidget(self.buttonBox)
+
+        self.setLayout(layout)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        
+        self.tabs = QTabWidget()
+        self.tabs.setDocumentMode(True)
+        self.tabs.tabBarDoubleClicked.connect(self.tab_open_doubleclick)
+        self.tabs.currentChanged.connect(self.current_tab_changed)
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_current_tab)
+
+        self.setCentralWidget(self.tabs)
+
+        self.status = QStatusBar()
+        self.setStatusBar(self.status)
+
+        navtb = QToolBar("Navigation")
+        navtb.setIconSize(QSize(16, 16))
+        self.addToolBar(navtb)
+
+        back_btn = QAction(QIcon(resource_path('images/arrow-180.png')), "Back", self)
+        back_btn.setStatusTip("Back to previous page")
+        back_btn.triggered.connect(lambda: self.tabs.currentWidget().back())
+        navtb.addAction(back_btn)
+
+        next_btn = QAction(QIcon(resource_path('images/arrow-000.png')), "Forward", self)
+        next_btn.setStatusTip("Forward to next page")
+        next_btn.triggered.connect(lambda: self.tabs.currentWidget().forward())
+        navtb.addAction(next_btn)
+
+        reload_btn = QAction(QIcon(resource_path('images/arrow-circle-315.png')), "Reload", self)
+        reload_btn.setStatusTip("Reload page")
+        reload_btn.triggered.connect(lambda: self.tabs.currentWidget().reload())
+        navtb.addAction(reload_btn)
+
+        home_btn = QAction(QIcon(resource_path('images/home.png')), "Home", self)
+        home_btn.setStatusTip("Go home")
+        home_btn.triggered.connect(self.navigate_home)
+        navtb.addAction(home_btn)
+
+        navtb.addSeparator()
+
+        self.httpsicon = QLabel()  # Yes, really!
+        self.httpsicon.setPixmap(QPixmap(resource_path('images/lock-nossl.png')))
+        navtb.addWidget(self.httpsicon)
+
+        self.urlbar = QLineEdit()
+        self.urlbar.returnPressed.connect(self.navigate_to_url)
+        self.urlbar.setStyleSheet(
+			"QLineEdit"
+				"{"
+					"border-radius: 10px;"
+					"border: 2px solid #b9b9b9;"
+  					"padding: 3px;"
+
+				"}"
+			)
+        navtb.addWidget(self.urlbar)
+
+        stop_btn = QAction(QIcon(resource_path('images/cross-circle.png')), "Stop", self)
+        stop_btn.setStatusTip("Stop loading current page")
+        stop_btn.triggered.connect(lambda: self.tabs.currentWidget().stop())
+        navtb.addAction(stop_btn)
+
+        # Uncomment to disable native menubar on Mac
+        # self.menuBar().setNativeMenuBar(False)
+
+        file_menu = self.menuBar().addMenu("&File")
+
+        new_tab_action = QAction(QIcon(resource_path('images/ui-tab-plus.png')), "New Tab", self)
+        new_tab_action.setStatusTip("Open a new tab")
+        new_tab_action.triggered.connect(lambda _: self.add_new_tab())
+        file_menu.addAction(new_tab_action)
+
+        open_file_action = QAction(QIcon(resource_path('images/disk-arrow.png')), "Open file...", self)
+        open_file_action.setStatusTip("Open from file")
+        open_file_action.triggered.connect(self.open_file)
+        file_menu.addAction(open_file_action)
+
+        save_file_action = QAction(QIcon(resource_path('images/disk-pencil.png')), "Save Page As...", self)
+        save_file_action.setStatusTip("Save current page to file")
+        save_file_action.triggered.connect(self.save_file)
+        file_menu.addAction(save_file_action)
+
+        self.add_new_tab(QUrl('https://seuss.fandom.com/wiki/Thneed'), 'Homepage')
+
+        self.show()
+
+        self.setWindowTitle("Thnoogle")
+        self.setWindowIcon(QIcon(resource_path('images/icon.ico')))
+
+    def add_new_tab(self, qurl=None, label="Blank"):
+
+        if qurl is None:
+            qurl = QUrl('https://seuss.fandom.com/wiki/Thneed')
+
+        browser = QWebEngineView()
+        browser.load(qurl)
+        browser.page().acceptNavigationRequest(qurl, 0, False)
+        i = self.tabs.addTab(browser, label)
+
+        self.tabs.setCurrentIndex(i)
+
+        # More difficult! We only want to update the url when it's from the
+        # correct tab
+        browser.urlChanged.connect(lambda qurl, browser=browser:
+                                   self.update_urlbar(qurl, browser))
+        browser.loadFinished.connect(lambda _, i=i, browser=browser:
+                                     self.tabs.setTabText(i, browser.page().title()))
+
+    def tab_open_doubleclick(self, i):
+        if i == -1:  # No tab under the click
+            self.add_new_tab()
+
+    def current_tab_changed(self, i):
+        qurl = self.tabs.currentWidget().url()
+        self.update_urlbar(qurl, self.tabs.currentWidget())
+        self.update_title(self.tabs.currentWidget())
+
+    def close_current_tab(self, i):
+        if self.tabs.count() < 2:
+            return
+
+        self.tabs.removeTab(i)
+
+    def update_title(self, browser):
+        if browser != self.tabs.currentWidget():
+            # If this signal is not from the current tab, ignore
+            return
+
+        title = self.tabs.currentWidget().page().title()
+        self.setWindowTitle("%s - Thnoogle" % title)
+
+    def about(self):
+        dlg = AboutDialog()
+        dlg.exec_()
+
+    def open_file(self):
+        filename, _ = QFileDialog.getOpenFileName(self, "Open file", "",
+                                                  "Hypertext Markup Language (*.htm *.html);;"
+                                                  "All files (*.*)")
+
+        if filename:
+            with open(filename, 'r') as f:
+                html = f.read()
+
+            self.tabs.currentWidget().setHtml(html)
+            self.urlbar.setText(filename)
+
+    def save_file(self):
+        filename, _ = QFileDialog.getSaveFileName(self, "Save Page As", "",
+                                                  "Hypertext Markup Language (*.htm *html);;"
+                                                  "All files (*.*)")
+
+        if filename:
+            html = self.tabs.currentWidget().page().toHtml()
+            with open(filename, 'w') as f:
+                f.write(html.encode('utf8'))
+
+    def print_page(self):
+        dlg = QPrintPreviewDialog()
+        dlg.paintRequested.connect(self.browser.print_)
+        dlg.exec_()
+
+    def navigate_home(self):
+        self.tabs.currentWidget().setUrl(QUrl("https://seuss.fandom.com/wiki/Thneed"))
+
+    def navigate_to_url(self):  # Does not receive the Url
+        q = QUrl(self.urlbar.text())
+        if q.url == "ExperimentalBrowser":
+            q.setUrl('https://cse.google.com/cse?cx=4d3c562badd7e4bd7')
+        if self.urlbar.text() == "":
+            q.setUrl(QUrl("https://seuss.fandom.com/wiki/Thneed"))
+        if q.scheme() == "":
+            q.setUrl(
+                'https://www.google.com/search?client=thneed&q=' + self.urlbar.text())
+        self.tabs.currentWidget().setUrl(q)
+
+    def update_urlbar(self, q, browser=None):
+
+        if browser != self.tabs.currentWidget():
+            # If this signal is not from the current tab, ignore
+            return
+
+        if q.scheme() == 'https':
+            # Secure padlock icon
+            self.httpsicon.setPixmap(QPixmap(resource_path('images/lock-ssl.png')))
+
+        else:
+            # Insecure padlock icon
+            self.httpsicon.setPixmap(QPixmap(resource_path('images/lock-nossl.png')))
+
+        self.urlbar.setText(q.toString())
+    def new_window(self, page_type):
+
+        profile = self.browser.page().profile()
+        new_page = QWebEnginePage(profile)
+        new_window = MainWindow()
+        new_window.browser.setPage(new_page)
+        new_window.show()
+
+        return new_window.browser
+
+
+app = QApplication(sys.argv)
+app.setApplicationName("Thnoogle")
+app.setOrganizationName("NoaDevINC")
+app.setOrganizationDomain("noadev.me")
+
+window = MainWindow()
+
+app.exec_()
